501a10d0458e2096e5632262d8e998471596d94c
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-02-05 12:55:46 -0500

    changed monitor_speed to 115200
    and ak test files

--- ElectricLongboardProject/src/main.cpp
+++ ElectricLongboardProject/src/main.cpp
@@ -1,11 +1,8 @@
-// #include "<stdio.h>"
 #include "esp_log.h"
 
 extern "C" {
     void app_main() {
-        esp_log_level_set("*", ESP_LOG_DEBUG);
         static const char* TAG = "MAIN";
         ESP_LOGD(TAG, "Hello world");
-        // printf("Hello world\n");
     }
 }
