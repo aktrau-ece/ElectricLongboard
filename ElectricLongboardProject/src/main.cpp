ec1708976eb9a55882ad4cb66d1646d3701dcf0a
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-02-19 14:48:36 -0500

    organizing files

--- ElectricLongboardProject/src/main.cpp
+++ ElectricLongboardProject/src/main.cpp
@@ -3,6 +3,6 @@
 extern "C" {
     void app_main() {
         static const char* TAG = "MAIN";
-        ESP_LOGD(TAG, "Hello world");
+        ESP_LOGD(TAG, "Hello world from the master branch");
     }
 }
