702ad1beb369cf9401ad5a3a206b16be2e7c99ab
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-03-18 18:52:32 -0400

    rpi bluetooth attempt

--- RaspiPyController/src/controller.py
+++ RaspiPyController/src/controller.py
@@ -1,23 +1,20 @@
 from pprint import pformat
 from time import sleep
 import logging
-from enum import Enum
+
+import bluetooth as bt
 
 log = logging.getLogger(__name__)
 
-PERIPHERAL_MAC_ADDRESS = ''
-
-class DriveMode(Enum):
-	FWD = 0 # front wheel drive
-	RWD = 1 # rear wheel drive
+PERIPHERAL_MAC_ADDRESS = '24:DC:C3:98:9E:F0'
 
 class Controller:
 
-	def __init__(self, drive_mode:DriveMode, **kwargs):
-		
-		self.drive_mode = drive_mode
+	def __init__(self, **kwargs):
 
-		# Check for bad arguments
+		self.traction_control = kwargs.get('traction_control', True)
+
+		# Check for bad arguments in kwargs
 		kwparams = []
 		for arg in kwargs:
 			if arg not in kwparams: log.error(f'Unknown argument: {arg}')
@@ -25,8 +22,22 @@ class Controller:
 
 	def start(self):
 		
-		pass
+		self.connectWithRemote()
 
 	def connectWithRemote(self, remote_mac=PERIPHERAL_MAC_ADDRESS):
 
-		pass
+		service_matches = bt.find_service( address=remote_mac )
+		if len(service_matches) == 0: log.error(f'Bluetooth peripheral not found')
+
+		log.debug(f'Available bluetooth peripherals: {pformat(service_matches)}')
+
+		service_match = service_matches[0] # pick the 1st match
+		port = service_match['port']
+		name = service_match['name']
+		host = service_match['host']
+
+		log.debug(f'Connecting to "{name}", on host {host}, through port {port}..')
+		sock = bt.BluetoothSocket(bt.RFCOMM)
+		connection_result = sock.connect((host, port))
+		log.debug(f'{connection_result}')
+		
