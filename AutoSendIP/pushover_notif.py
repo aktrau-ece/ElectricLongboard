856ad4f2e618929ee69b5e2aba0a5ee0d921cd61
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-03-21 13:37:43 -0400

    Update pushover_notif.py

--- AutoSendIP/pushover_notif.py
+++ AutoSendIP/pushover_notif.py
@@ -10,7 +10,7 @@ user_key = 'usiy1jsb5zdx1pffknnd5nqre5sifd'
 api_token = 'abmpxzwfgzyd52vhipw2vjycn6a5og'
 
 # Wait for RPi to connect to the internet
-sleep(10)
+sleep(20)
 
 ip_addr = subprocess.check_output(['hostname', '-I']).decode('utf-8')
 print(f'Hostname: {ip_addr}')
