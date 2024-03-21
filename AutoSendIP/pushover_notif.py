130921786d494e1747fe8b98b9c849510004a14c
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-03-21 13:29:14 -0400

    Update pushover_notif.py

--- AutoSendIP/pushover_notif.py
+++ AutoSendIP/pushover_notif.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from pprint import pformat
 import subprocess
 from time import sleep
@@ -7,11 +9,11 @@ from pushover import Client
 user_key = 'usiy1jsb5zdx1pffknnd5nqre5sifd'
 api_token = 'abmpxzwfgzyd52vhipw2vjycn6a5og'
 
-ip_addr = subprocess.check_output(['hostname', '-I']).decode('utf-8')
-print(f'Hostname: {ip_addr}')
-
 # Wait for RPi to connect to the internet
 sleep(10)
+
+ip_addr = subprocess.check_output(['hostname', '-I']).decode('utf-8')
+print(f'Hostname: {ip_addr}')
 
 client = Client(user_key, api_token=api_token)
 client.send_message(f'Here is my IP address: {ip_addr}', title='RPi Login')
