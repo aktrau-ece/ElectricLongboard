d25474c437df9fe0deab1c58d206a036c567c2d8
aktrau-ece <ak.tranau@gmail.com>
2024-03-24 16:52:09 -0400

    Update hall_sensor.py

--- RaspiPyController/src/tests/hall_sensor.py
+++ RaspiPyController/src/tests/hall_sensor.py
@@ -1,6 +1,6 @@
 from pprint import pformat
 from time import sleep
-import logging
+import logging, logging.config
 
 import RPi.GPIO as GPIO
 
@@ -22,3 +22,36 @@ def testHallSensor():
 		sleep(0.5)
 
 	GPIO.cleanup()
+
+if __name__ == '__main__':
+
+	LOGGING_CONFIG = {
+		'version': 1,
+		'disable_existing_loggers' : False,
+
+		'loggers': {
+			'main': {
+				'handlers': ['console'],
+				'level': 'DEBUG'
+			}
+		},
+
+		'handlers': {
+			'console': {
+				'level': 'DEBUG',
+				'formatter': 'console',
+				'class': 'logging.StreamHandler',
+				'stream': 'ext://sys.stdout'
+			}
+		},
+
+		'formatters': {
+			'standard': { 'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s' },
+			'console': { 'format': '[%(levelname)s] %(name)s: %(message)s' }
+		},
+	}
+
+	logging.config.dictConfig(LOGGING_CONFIG)
+	log = logging.getLogger('main')
+
+	testHallSensor()
