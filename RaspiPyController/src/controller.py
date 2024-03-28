8965e9ac5ff23d2f287cf9692ee5ba33ad5371a7
aktrau-ece <121685865+aktrau-ece@users.noreply.github.com>
2024-03-28 16:30:27 -0400

    Update controller.py

--- RaspiPyController/src/controller.py
+++ RaspiPyController/src/controller.py
@@ -72,7 +72,8 @@ class RemoteControl(threading.Thread):
 		self.log.info(f'Reading messages..')
 
 		while True:
-			data = sock.recv(self.size).encode('utf-8')
+			raw_data = sock.recv(self.size)
+			data = int(raw_data.decode('utf-8'))
 			if data:
 				self.joystick_buffer.put(data)
 				self.log.debug(f'Joystick position buffer: {str(self.joystick_buffer)}')
