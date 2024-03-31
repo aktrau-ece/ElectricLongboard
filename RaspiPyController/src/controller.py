e45f9aedc6bb1f395f07acc86cae92cbe91e8bea
aktrau-ece <ak.tranau@gmail.com>
2024-03-31 15:00:21 -0400

    refactoring for  remote control communication

--- RaspiPyController/src/controller.py
+++ RaspiPyController/src/controller.py
@@ -1,7 +1,7 @@
 from pprint import pformat
 from time import sleep
 import logging
-from queue import Queue
+from collections import deque
 import threading
 
 import RPi.GPIO as GPIO
@@ -48,59 +48,39 @@ class RemoteControl(threading.Thread):
 
 	'''
 	Params:
-		`joystick_buffer`: Buffer used to keep a short history of joystick position data, intended for
-			smoothening any abrupt throttle changes
-		`joystick_buffer_lock`: Mutex used for making changes to `joystick_buffer`
 		`name`: The name of this thread (used for logging) (default is "controller:remote")
 		`periph_macaddr`: MAC address of the remote control
 		`size`: Bluetooth receive size (default is 1024)
 		`log`: Optional logger. If not specified, a logger with the name given in the `name` param will
 			be created and used
 	'''
-	def __init__(self, joystick_buffer:Queue, joystick_buffer_lock:threading.Lock, **kwargs):
+	def __init__(self, **kwargs):
+
+		threading.Thread.__init__(self)
 
 		kwparams = ['name', 'periph_macaddr', 'size', 'log']
 		for arg in kwargs:
 			if arg not in kwparams: log.error(f'Unknown argument: {arg}')
 
-		threading.Thread.__init__(self)
-		self.joystick_buffer = joystick_buffer
-		self.joystick_buffer_lock = joystick_buffer_lock
 		self.name = kwargs.get('name', 'controller:remote')
 		self.periph_macaddr = kwargs.get('periph_macaddr', PERIPHERAL_MAC_ADDRESS)
 		self.size = kwargs.get('size', 1024)
 		self.log = kwargs.get('log', logging.getLogger(self.name))
 
+
+		self.joys_pos_buffer = deque([0 for i in range(10)], maxlen=10) # joystick position buffer.
+			# circular buffer used to keep a short history of joystick position data - intended 
+			# to be used for smoothening any abrupt throttle changes. It can take values from 0 to 100
+
+		self.joys_pos_buffer_lock = threading.Lock() # mutex for managing changes to `joys_pos_buffer`
+
 	def run(self):
 
 		self.log.info('Here!')
 
-		self.log.info(f'Scanning for bluetooth services..')
-		service_matches = bluetooth.find_service(address=self.periph_macaddr)
-		self.log.info('Found services: ' + pformat(service_matches) )
-		first_match = service_matches[0]
-		port = first_match["port"]
-		name = first_match["name"]
-		host = first_match["host"]
+		port, name, host = self.findService()
+		sock = self.connectToClient(port, name, host)
 
-		self.log.info(f'Connecting to client..')
-		sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-		sock.connect((host, port))
-
-		self.log.info(f'Reading messages..')
-
-		while True:
-			raw_data = sock.recv(self.size)
-			data = int(raw_data.decode('utf-8'))
-
-			self.joystickBufferForcePush(data)
-			self.log.debug(f'Joystick position buffer: {list(self.joystick_buffer.queue)}')
-
-		sock.close()
-
-	'''
-	Scan for any nearby bluetooth peripherals
-	'''
 	def scanForDevices(self):
 
 		self.log.info(f'Scanning for bluetooth peripherals..')
@@ -108,11 +88,46 @@ class RemoteControl(threading.Thread):
 		devices_unpacked = [f'{name} | {addr} | {_class}' for addr, name, _class in available_devices]
 		self.log.info( 'Found devices: ' + pformat(devices_unpacked) )
 
-	def joystickBufferForcePush(self, item):
+	def findService(self):
 
-		self.joystick_buffer_lock.acquire(blocking=1)
+		self.log.info(f'Scanning for bluetooth services..')
 
-		if self.joystick_buffer.full(): self.joystick_buffer.get()
-		self.joystick_buffer.put(item)
+		service_matches = bluetooth.find_service(address=self.periph_macaddr)
+		self.log.info('Found services: ' + pformat(service_matches) )
 
-		self.joystick_buffer_lock.release()
+		first_match = service_matches[0]
+		port = first_match["port"]
+		name = first_match["name"]
+		host = first_match["host"]
+
+		return port, name, host
+
+	def connectToClient(self, port, name, host):
+
+		self.log.info(f'Connecting to client..')
+		sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		sock.connect((host, port))
+
+		return sock
+
+	def readMessages(self, sock):
+
+		self.log.info(f'Reading messages..')
+
+		while True:
+			data = sock.recv(self.size)
+			joystick_pos = int(data.decode('utf-8'))
+
+			self.pushJoystickBuffer(joystick_pos)
+			self.log.debug(f'Joystick position buffer: {self.getJoystickBufferAsList()}')
+
+		sock.close()
+
+	def pushJoystickBuffer(self, pos:int):
+
+		with self.joys_pos_buffer_lock:
+			self.joys_pos_buffer.append(pos)
+
+	def getJoystickBufferAsList(self):
+
+		return list(self.joys_pos_buffer)
